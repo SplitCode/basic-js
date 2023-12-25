022eb20daa9f55255a24cbd09e621a09e96a1680
SplitCode <natacsp@mail.ru>
2023-12-25 13:42:27 +0300

    feat: resolve dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let currentDomain = '';
+
+    for (const part of parts) {
+      currentDomain += `.${part}`;
+
+      if (!stats[currentDomain]) {
+        stats[currentDomain] = 1;
+      } else {
+        stats[currentDomain]++;
+      }
+    }
+  }
+
+  return stats;
 }
 
 module.exports = {
